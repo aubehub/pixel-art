2484b0390e1226733d4e39dee1a0340eb4a09615
aubehub <albalopsanchez@gmail.com>
2020-07-14 00:44:31 -0300

    Updates

---
+++ precache-manifest.0c38699e2b2f934f3703eb12e693890f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8650def43b7dc320380504e00eaaea56",
+    "url": "/pixel-art/index.html"
+  },
+  {
+    "revision": "dafa8e0dc059ea71597f",
+    "url": "/pixel-art/static/css/main.bd679250.chunk.css"
+  },
+  {
+    "revision": "c4c0b4bffd3667ae1c1a",
+    "url": "/pixel-art/static/js/2.5ec89ef2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pixel-art/static/js/2.5ec89ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dafa8e0dc059ea71597f",
+    "url": "/pixel-art/static/js/main.c9a9b12f.chunk.js"
+  },
+  {
+    "revision": "de3c8e138ab6dcafd95e",
+    "url": "/pixel-art/static/js/runtime-main.7f676ac3.js"
+  }
+]);
